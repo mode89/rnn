cd25c15eaec237d05f8f0287c54bb10346518f43
Mode <mode89@mail.ru>
2019-01-04 00:56:49 +0800

    Parse arguments

--- label.py
+++ label.py
@@ -1,5 +1,12 @@
+import argparse
+
 def main():
-    pass
+    args = parse_arguments()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
