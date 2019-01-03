f53c68527eec8d13d6e6a693570e1e4582a0d575
Mode <mode89@mail.ru>
2019-01-03 23:00:33 +0800

    Print words

--- record.py
+++ record.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 VOCABULARY = [
     "cancel",
@@ -10,6 +11,7 @@ VOCABULARY = [
 
 def main():
     listOfWords = random_list_of_words()
+    print_words(listOfWords)
 
 def random_list_of_words():
     wordNum = 100 / len(VOCABULARY)
@@ -18,6 +20,18 @@ def random_list_of_words():
         listOfWords += [word] * wordNum
     random.shuffle(listOfWords)
     return listOfWords
+
+def print_words(words):
+    counter = 0
+    wait_for(3)
+    for word in words:
+        print("{} {}".format(counter, word))
+        counter += 1
+        pause = random.uniform(4, 8)
+        wait_for(pause)
+
+def wait_for(seconds):
+    time.sleep(seconds)
 
 if __name__ == "__main__":
     main()
