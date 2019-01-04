cde7b40167979835f90a372660401b95ecd73b53
Mode <mode89@mail.ru>
2019-01-05 01:58:57 +0800

    Load features and labels

--- training_data.py
+++ training_data.py
@@ -1,4 +1,7 @@
 import argparse
+import json
+import numpy
+import os
 
 class TrainingData:
 
@@ -11,5 +14,21 @@ class TrainingData:
         args = parser.parse_args()
         self.directory = args.directory
 
+    def prepare(self):
+        self.load_features()
+        self.load_labels()
+
+    def load_features(self):
+        mfccPath = os.path.join(self.directory, "mfcc.npz")
+        mfcc = numpy.load(mfccPath)
+        self.chunk_size = mfcc["chunk_size"]
+        self.features = mfcc["features"]
+
+    def load_labels(self):
+        labelsPath = os.path.join(self.directory, "labels.json")
+        with open(labelsPath, "r") as labelsFile:
+            self.labels = json.load(labelsFile)
+
 if __name__ == "__main__":
     trainingData = TrainingData()
+    trainingData.prepare()
