891d9c705641613f1a2c769c4cfe419f817bdda2
Mode <mode89@mail.ru>
2019-01-06 02:00:29 +0800

    Generate inputs and outputs

--- training_data.py
+++ training_data.py
@@ -22,19 +22,40 @@ class TrainingData:
     def prepare(self):
         self.load_features()
         self.load_labels()
+        self.generate_data()
+
+    def generate_data(self):
         sampleLength = \
             int(SAMPLE_LENGTH * FRAME_RATE / self.chunk_size)
         activationLength = \
             int(ACTIVATION_LENGTH * FRAME_RATE / self.chunk_size)
+        self.inputs = list()
+        self.outputs = list()
         for label in self.labels:
             utteranceBegin = label["begin"] / self.chunk_size
             utteranceEnd = label["end"] / self.chunk_size
             utteranceLength = utteranceEnd - utteranceBegin
             pauseLength = sampleLength - utteranceLength - activationLength
-            halfPause = int(pauseLength / 2)
-            sampleBegin = utteranceBegin - halfPause
+            prePause = int(pauseLength / 2)
+            postPause = pauseLength - prePause
+            sampleBegin = utteranceBegin - prePause
             sampleEnd = sampleBegin + sampleLength
             inputs = self.features[sampleBegin:sampleEnd,:]
+            self.inputs.append(inputs)
+            outputs = numpy.vstack((
+                numpy.tile(
+                    single_output_from_label(None),
+                    [prePause + utteranceLength, 1]),
+                numpy.tile(
+                    single_output_from_label(label["word"]),
+                    [activationLength, 1]),
+                numpy.tile(
+                    single_output_from_label(None),
+                    [postPause, 1])
+            ))
+            self.outputs.append(outputs)
+        self.inputs = numpy.array(self.inputs)
+        self.outputs = numpy.array(self.outputs)
 
     def load_features(self):
         mfccPath = os.path.join(self.directory, "mfcc.npz")
@@ -46,6 +67,13 @@ class TrainingData:
         labelsPath = os.path.join(self.directory, "labels.json")
         with open(labelsPath, "r") as labelsFile:
             self.labels = json.load(labelsFile)
+
+def single_output_from_label(word):
+    vocabulary = VOCABULARY + [None]
+    output = numpy.zeros(len(vocabulary))
+    index = vocabulary.index(word)
+    output[index] = 1.0
+    return output
 
 if __name__ == "__main__":
     trainingData = TrainingData()
