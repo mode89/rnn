3996037c7e349e2d08864daf460c105ae680daa7
Mode <mode89@mail.ru>
2019-01-06 02:15:20 +0800

    Load samples

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import argparse
+import numpy
+import os
 
 class Model:
 
@@ -10,6 +12,15 @@ class Model:
         parser.add_argument("directory")
         args = parser.parse_args()
         self.directory = args.directory
+
+    def load_samples(self):
+        print("Loading samples ...")
+        samplesPath = os.path.join(self.directory, "samples.npz")
+        data = numpy.load(samplesPath)
+        class Samples: pass
+        self.samples = Samples()
+        self.samples.inputs = data["inputs"]
+        self.samples.outputs = data["outputs"]
 
 if __name__ == "__main__":
     model = Model()
