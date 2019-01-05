90f14b83463750b7eca5b71a0068efd912539e73
Mode <mode89@mail.ru>
2019-01-06 02:24:08 +0800

    Build model

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import argparse
 import numpy
 import os
+import tensorflow as tf
+
+SEQUENCE_SIZE = 20
+BATCH_SIZE = 1
 
 class Model:
 
@@ -22,5 +26,42 @@ class Model:
         self.samples.inputs = data["inputs"]
         self.samples.outputs = data["outputs"]
 
+    def build(self):
+        self.load_samples()
+        print("Building model ...")
+
+        tf.set_random_seed(0)
+
+        inputsNum = self.samples.inputs.shape[2]
+        self.inputs = tf.placeholder(tf.float32,
+            (None, SEQUENCE_SIZE, inputsNum), name="inputs")
+        outputsNum = self.samples.outputs.shape[2]
+        self.outputs = tf.placeholder(tf.float32,
+            (None, SEQUENCE_SIZE, outputsNum), name="outputs")
+
+        lstmCell = tf.nn.rnn_cell.LSTMCell(num_units=128)
+        zeroState = lstmCell.zero_state(BATCH_SIZE, tf.float32)
+        initialState = tf.nn.rnn_cell.LSTMStateTuple(
+            c=tf.placeholder(tf.float32, (None, lstmCell.state_size.c)),
+            h=tf.placeholder(tf.float32, (None, lstmCell.state_size.h)))
+        lstmOutputs, finalState = tf.nn.dynamic_rnn(
+            cell=lstmCell,
+            inputs=self.inputs,
+            sequence_length=[SEQUENCE_SIZE],
+            initial_state=initialState)
+        logits = list()
+        denseLayer = tf.layers.Dense(outputsNum, tf.sigmoid)
+        for i in range(SEQUENCE_SIZE):
+            tempLogits = denseLayer(lstmOutputs[:, i, :])
+            logits.append(tempLogits)
+        logits = tf.reshape(logits, tf.shape(self.outputs))
+        lossOp = tf.losses.softmax_cross_entropy(self.outputs, logits)
+        optimizer = tf.train.AdamOptimizer()
+        trainOp = optimizer.minimize(lossOp)
+        logitsSoftmax = tf.nn.softmax(logits)
+        mseOp = tf.losses.mean_squared_error(self.outputs, logits)
+        diffOp = tf.losses.absolute_difference(self.outputs, logits)
+
 if __name__ == "__main__":
     model = Model()
+    model.build()
